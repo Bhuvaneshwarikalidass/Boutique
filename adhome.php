a029400a51f80fd0c65900fa995806153943f9c3
bhuvaneshwari <bhuvaneshwarikalidass@gmail.com>
2019-10-23 19:34:49 +0530

    first

---
+++ adhome.php
@@ -0,0 +1,4 @@
+<?php
+include('index.php');
+echo "<center><h1 style = 'color:blue'>WELCOME</h1></center>";
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>BOUTIQUES</title>
+</head>
+<link href="style.css" rel="stylesheet">
+<body>
+    <!-- nav start -->
+    <div class="navbar">
+        <a href="index.php">HOME</a>
+        <a href="user.php">USER</a>
+        <a href="admin.php">ADMIN</a>
+        <a href="register.php">REGISTER</a>
+    </div>  
+    <!-- nav end -->
+
+     <!-- second navabar start -->
+     <div class="friend">
+        <center><h1>FRIENDS BOUTIQUES</h1></center>
+    </div>  
+    <!-- second navbar end -->
+
+    <!-- image start -->
+        <center>
+        <img src="dress.jpg" height="360px" weight="360px">
+        </center>
+    <!-- end image -->
+ 
+   
+</body>
